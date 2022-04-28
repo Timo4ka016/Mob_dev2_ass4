ea02ec82cfb19be57ff59e45b84732be6a6c5ce6
Timo4ka <82034033+Timo4ka016@users.noreply.github.com>
2022-04-28 23:43:32 +0600

    d

---
+++ Ass4_MobDev/app/src/main/java/dts/apps/ass4_mobdev/MainActivity.kt
@@ -0,0 +1,11 @@
+package dts.apps.ass4_mobdev
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
