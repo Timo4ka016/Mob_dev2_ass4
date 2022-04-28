af7f5ae07e5c6cc7ba5a47337dd2ab37795e2490
Timo4ka <82034033+Timo4ka016@users.noreply.github.com>
2022-04-29 00:47:05 +0600

    d

--- Ass4_MobDev/app/src/main/java/dts/apps/ass4_mobdev/MainActivity.kt
+++ Ass4_MobDev/app/src/main/java/dts/apps/ass4_mobdev/MainActivity.kt
@@ -1,11 +1,44 @@
 package dts.apps.ass4_mobdev
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import dts.apps.ass4_mobdev.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        onListener()
     }
+
+    private fun onListener() {
+        val clickable : List<View> = listOf(
+            binding.firstBox , binding.secondBox , binding.thirdBox , binding.fourthBox ,
+            binding.fifthBox , binding.mainActLayout , binding.btnRed , binding.btnBlue , binding.btnGreen
+        )
+
+        for (item in clickable) {
+            item.setOnClickListener { setColor(it) }
+        }
+    }
+
+    private fun setColor(view : View) {
+        when(view.id) {
+            R.id.first_box -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.second_box -> view.setBackgroundColor(Color.GRAY)
+            R.id.third_box -> view.setBackgroundColor(Color.BLUE)
+            R.id.fourth_box -> view.setBackgroundColor(Color.MAGENTA)
+            R.id.fifth_box -> view.setBackgroundColor(Color.BLUE)
+
+            R.id.btn_blue -> binding.thirdBox.setBackgroundResource(R.color.blue_)
+            R.id.btn_red -> binding.fourthBox.setBackgroundResource(R.color.red_)
+            R.id.btn_green -> binding.fifthBox.setBackgroundResource(R.color.green_)
+        }
+    }
+
 }
